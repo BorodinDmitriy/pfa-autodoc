a10f0d58440a4225d86bcc77985b57c6dec4a993
Borodin Dmitriy <demansen@yandex.ru>
2019-09-25 23:29:46 +0300

    initialization of swagger, liquibase, postgresql and spring boot application

---
+++ src/main/java/com/autodoc/pfa/pfaautodoc/Application.java
@@ -0,0 +1,13 @@
+package com.autodoc.pfa.pfaautodoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
