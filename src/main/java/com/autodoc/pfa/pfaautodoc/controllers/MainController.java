8043a859aa2791ad83aca8a49671a4a750cbc681
Borodin Dmitriy <demansen@yandex.ru>
2019-09-30 23:17:03 +0300

    addition of thymeleaf to application

---
+++ src/main/java/com/autodoc/pfa/pfaautodoc/controllers/MainController.java
@@ -0,0 +1,27 @@
+package com.autodoc.pfa.pfaautodoc.controllers;
+
+import com.autodoc.pfa.pfaautodoc.services.IDocumentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+
+    @Autowired
+    public MainController() { }
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String mainPage(HttpServletRequest request, Model model) {
+        return "index";
+    }
+}
